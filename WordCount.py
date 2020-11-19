de379b2b7a3b6634530a7862be5e05bb932a8384
Allistair1 <71676591+Allistair1@users.noreply.github.com>
2020-11-18 17:07:26 -0800

    Update WordCount.py

--- WordCount.py
+++ WordCount.py
@@ -23,7 +23,7 @@ def U():
     print(turtle.pos()[0])
     x = turtle.pos()[0]
     y = turtle.pos()[1]
-    turtle.write(wordcount("Enter the equation below and hit submit. If you need to fix your work, go back to the prior screen")), font = style)
+    turtle.write(wordcount("Enter the equation below and hit submit. If you need to fix your work, go back to the prior screen"), font = style)
     turtle.penup()
     turtle.setpos(x + 23, y)
     turtle.pendown()
