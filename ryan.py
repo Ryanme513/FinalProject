24931c6ab58b2d85b8f95af9a677ff11f68827e1
Ryanme513 <71676257+Ryanme513@users.noreply.github.com>
2020-12-02 18:18:22 -0800

    updated

--- ryan.py
+++ ryan.py
@@ -44,16 +44,16 @@ def choose_index(options):
 #   print(f'Playing {track["name"]} by {track["artists"][0]["name"]}...')
 #   webbrowser.open(track['external_urls']['spotify'])
 
-def play_song(tracks):
-  # print(f'Searching for a few songs from "{artist_id}"...')
-  # recommendations = spotify.recommendations(seed_artists=[artist_id], limit=10)
-  # # Filter out tracks without Spotify URLs
-  # tracks = [t for t in recommendations['tracks'] if 'spotify' in t['external_urls']]
-  track_titles = [t['artists'][0]['name'] + ' - ' + t['name'] for t in tracks]
-  print(f'I found a few songs from ARTIST for you:')
-  track = tracks[choose_index(track_titles)]
-  print(f'Playing {track["name"]} by {track["artists"][0]["name"]}...')
-  webbrowser.open(track['external_urls']['spotify'])
+# def play_song(tracks):
+#   # print(f'Searching for a few songs from "{artist_id}"...')
+#   # recommendations = spotify.recommendations(seed_artists=[artist_id], limit=10)
+#   # # Filter out tracks without Spotify URLs
+#   # tracks = [t for t in recommendations['tracks'] if 'spotify' in t['external_urls']]
+#   track_titles = [t['artists'][0]['name'] + ' - ' + t['name'] for t in tracks]
+#   print(f'I found a few songs from ARTIST for you:')
+#   track = tracks[choose_index(track_titles)]
+#   print(f'Playing {track["name"]} by {track["artists"][0]["name"]}...')
+#   webbrowser.open(track['external_urls']['spotify'])
 
 # # For Ramiro: get('C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s')
 
@@ -69,22 +69,23 @@ def play_song(tracks):
 # if __name__ == '__main__':
 #   main()
 
-# artists = {
-#     Marshmellow=https://open.spotify.com/artist/64KEffDW9EtZ1y2vBYgq8T,
-#     Khalid=https://open.spotify.com/artist/6LuN9FCkKOj5PcnpouEgny,
-#     Ariana_Grande=https://open.spotify.com/artist/66CXWjxzNUsdJxJ2JdwvnR,
-#     Cardi_B=https://open.spotify.com/artist/4kYSro6naA4h99UJvo89HB,
-#     Ed_Sheeran=https://open.spotify.com/artist/6eUKZXaKkcviH0Ku9w2n3V,
-#     Justin_Bieber=https://open.spotify.com/artist/1uNFoZAHBGtllmzznpCI3s,
-#     Dua_Lipa=https://open.spotify.com/artist/6M2wZ9GZgrQXHCFfjv46we,
-#     J_Balvin=https://open.spotify.com/artist/1vyhD5VmyZ7KMfW5gqLgo5,
-#     Drake=https://open.spotify.com/artist/3TVXtAsR1Inumwj472S9r4,
-#     The_Weeknd=https://open.spotify.com/artist/1Xyo4u8uXC1ZmMpatF05PJ
+artists = {
+    'Marshmellow':'https://open.spotify.com/artist/64KEffDW9EtZ1y2vBYgq8T',
+    'Khalid':'https://open.spotify.com/artist/6LuN9FCkKOj5PcnpouEgny',
+    'Ariana_Grande':'https://open.spotify.com/artist/66CXWjxzNUsdJxJ2JdwvnR',
+    'Cardi_B':'https://open.spotify.com/artist/4kYSro6naA4h99UJvo89HB',
+    'Ed_Sheeran':'https://open.spotify.com/artist/6eUKZXaKkcviH0Ku9w2n3V',
+    'Justin_Bieber':'https://open.spotify.com/artist/1uNFoZAHBGtllmzznpCI3s',
+    'Dua_Lipa':'https://open.spotify.com/artist/6M2wZ9GZgrQXHCFfjv46we',
+    'J_Balvin':'https://open.spotify.com/artist/1vyhD5VmyZ7KMfW5gqLgo5',
+    'Drake':'https://open.spotify.com/artist/3TVXtAsR1Inumwj472S9r4',
+    'The_Weeknd':'https://open.spotify.com/artist/1Xyo4u8uXC1ZmMpatF05PJ'
+}
 
-# }
-def top_artist():
+print(choose_index(artists))
+def top_artist():1
   response = input('Which artist would you like to see?')
   x = spotify.artist_top_tracks(artist_id="https://open.spotify.com/artist/3TVXtAsR1Inumwj472S9r4")
   play_song(x['tracks'])
 
-top_artist()
+# top_artist()
