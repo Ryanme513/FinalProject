9880c294943ce9e8bfc1b0a0fca354ce053022e3
alexanduh <alexander.j.peng@gmail.com>
2020-12-02 18:32:33 -0800

    Update ryan.py

--- ryan.py
+++ ryan.py
@@ -82,16 +82,15 @@ artists = {
     'The Weeknd':'https://open.spotify.com/artist/1Xyo4u8uXC1ZmMpatF05PJ'
 }
 
-artists_names = list(artists.keys())
 
-artist_name_index = choose_index(artists_names)
-
-chosen_artist = artists_names[artist_name_index]
-
-print(artists[chosen_artist])
 
 # print(choose_index(artists))
 def top_artist():
+  artists_names = list(artists.keys())
+  artist_name_index = choose_index(artists_names)
+  chosen_artist = artists_names[artist_name_index]
+  print(artists[chosen_artist])
+  
   response = input('Which artist would you like to see?')
   x = spotify.artist_top_tracks(artist_id="https://open.spotify.com/artist/3TVXtAsR1Inumwj472S9r4")
   play_song(x['tracks'])
